dda669a2a3627a8b784675f068d03aaf8341721d
gifino1 <gifino@trisquare.com.my>
2023-11-23 20:38:26 +0700

    divide router

--- src/app/(routes)/api/auth/[...nextauth]/route.ts
+++ src/app/(routes)/api/auth/[...nextauth]/route.ts
@@ -1,38 +1,5 @@
-import connectDb from "@/app/_lib/connect-db";
-import CredentialsProvider from "next-auth/providers/credentials";
-import User from "@/app/_models/User";
-import { compare } from "bcryptjs";
-import NextAuth, { NextAuthOptions } from "next-auth";
-
-export const authOptions: NextAuthOptions = {
-  providers: [
-    CredentialsProvider({
-      id: "credentials",
-      name: "Credentials",
-      credentials: {
-        username: { label: "Username", type: "text" },
-        password: { label: "Password", type: "password" },
-      },
-      async authorize(credentials: any) {
-        await connectDb();
-        try {
-          const user = await User.findOne({ email: credentials.email });
-          if (user) {
-            const isPasswordCorrect = await compare(
-              credentials.password,
-              user.password
-            );
-            if (isPasswordCorrect) {
-              return user;
-            }
-          }
-        } catch (err: any) {
-          throw new Error(err);
-        }
-      },
-    }),
-  ],
-};
+import { authOptions } from "@/app/_lib/AuthOptions";
+import NextAuth from "next-auth";
 
 export const handler = NextAuth(authOptions);
 export { handler as GET, handler as POST };
