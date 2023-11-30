44397f6fa550fea6306ffa6fce6dfd89dc719be0
gifino1 <gifino@trisquare.com.my>
2023-11-30 15:39:06 +0700

    reponsive materi belajar

--- src/app/(routes)/materi-belajar/page.tsx
+++ src/app/(routes)/materi-belajar/page.tsx
@@ -2,7 +2,7 @@ import { Button } from "@/app/_ui/Button";
 import { CardHorizontal } from "@/app/_ui/CardHorizontal";
 export default function materiBelajar() {
   return (
-    <div className="max-w-screen-xl mx-auto px-4">
+    <div className="max-w-7xl mx-auto px-4">
       <div className="w-full mx-auto text-center">
         <h1 className="text-3xl font-bold text-blue-dark">Materi Belajar</h1>
         <h3 className="text-xl">Kelas 10</h3>
--- src/app/_ui/CardHorizontal.tsx
+++ src/app/_ui/CardHorizontal.tsx
@@ -9,23 +9,23 @@ interface CardComponent {
 
 export const CardHorizontal: FC<CardComponent> = ({ title, subTitle, url }) => {
   return (
-    <div className="w-full flex flex-row mt-12" style={{ height: 242 }}>
-      <div className="basis-1/3 bg-[url('/images/subbab.png')] bg-cover bg-center"></div>
-      <div className="basis-2/3 flex flex-col justify-between p-4">
-        <div>
-          <h3 className="text-blue-light text-base font-bold mb-1">
+    <div className="w-full flex flex-col sm:flex-row mt-12 h-[360px] sm:h-[242px]">
+      <div className="basis-1/2 sm:basis-1/3 bg-[url('/images/subbab.png')] bg-cover bg-center"></div>
+      <div className="basis-1/2 sm:basis-2/3 flex flex-col sm:justify-between py-4 sm:py-0 sm:px-4">
+        <div className=" ">
+          <h3 className="text-blue-light text-sm md:text-base font-bold mb-1">
             Pengenalan Konstruksi
           </h3>
-          <h2 className="text-blue-dark text-xl font-bold mb-1">
+          <h2 className="text-blue-dark text-base md:text-xl font-bold mb-1">
             Judul Sub Bab{" "}
           </h2>
-          <span className="block max-w-[600px]">
+          <span className="block text-base max-w-[600px] truncate sm:whitespace-normal">
             We focus on ergonomics and meeting you where you work. It's only a
             keystroke away. We focus on ergonomics and meeting you where you
             work. It's only a keystroke away.
           </span>
         </div>
-        <div>
+        <div className="mt-4 sm:mt-0">
           <Button style="outline">Pelajari Lanjut</Button>
         </div>
       </div>
