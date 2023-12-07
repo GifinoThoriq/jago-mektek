3cc4997b4caddfacef76fb5644dc15ed84bc418d
gifino1 <gifino@trisquare.com.my>
2023-12-07 15:33:21 +0700

    fix build

--- src/app/(routes)/api/materi/route.ts
+++ src/app/(routes)/api/materi/route.ts
@@ -1,12 +1,10 @@
 import Materi from "@/app/_models/Materi";
 import connectDb from "@/app/_lib/connect-db";
-import { hash } from "bcryptjs";
-import type { NextApiRequest, NextApiResponse } from "next";
 import { NextResponse } from "next/server";
 
-export async function GET() {
+export const GET = async () => {
   await connectDb();
 
   const materis = await Materi.find();
   return NextResponse.json({ materis });
-}
+};
