ce5891c397a72c0b30b4316974b2fdea21d008ff
gifino1 <gifino@trisquare.com.my>
2024-07-22 11:24:26 +0700

    chore: update imported

--- src/lib/edgestore.ts
+++ src/lib/edgestore.ts
@@ -1,6 +1,7 @@
-"use client"
+"use client";
 
-import { type EdgeStoreRouter } from "../(routes)/api/edgestore/[...edgestore]/route";
+import { type EdgeStoreRouter } from "@/app/(routes)/api/edgestore/[...edgestore]/route";
 import { createEdgeStoreProvider } from "@edgestore/react";
 
-export const { EdgeStoreProvider, useEdgeStore } = createEdgeStoreProvider<EdgeStoreRouter>();
+export const { EdgeStoreProvider, useEdgeStore } =
+  createEdgeStoreProvider<EdgeStoreRouter>();
